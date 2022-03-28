f3734101cec3983fe229c40ef563d741aac1d9c8
Connor <2470724c@student.gla.ac.uk>
2022-03-28 02:04:55 +0100

    added admin.py

--- jot_legacy/jot/admin.py
+++ jot_legacy/jot/admin.py
@@ -1,10 +1,17 @@
 from django.contrib import admin
 import os 
 import django
-from jot.models import Users, Book, Review, Admin, UserProfile
+from jot.models import Book, Review, Category, UserProfile
+from django.contrib.auth.models import User
 
-admin.site.register(Users)
-admin.site.register(Book)
-admin.site.register(Review)
-admin.site.register(Admin)
+class BookAdmin(admin.ModelAdmin):
+    list_display = ('book_title', 'book_category', 'author')
+
+class ReviewAdmin(admin.ModelAdmin):
+    list_display=('reviewer', 'review_book', 'review_content')
+
+
+admin.site.register(Book, BookAdmin)
+admin.site.register(Review, ReviewAdmin)
+admin.site.register(Category)
 admin.site.register(UserProfile)
